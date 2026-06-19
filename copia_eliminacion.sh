8c1fba598a295eb4c62eef0275aa2d124c71f208
PedroNavinta <pedronavinta@gmail.com>
2026-06-19 00:23:39 -0500

    Add script to copy and delete log files

--- copia_eliminacion.sh
+++ copia_eliminacion.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+# Directorios
+ORIGEN="/var/log"
+DESTINO="/home/estudiante/log"
+# Crear directorio destino si no existe
+mkdir -p "$DESTINO"
+# Copiar archivos
+cp -r "$ORIGEN"/* "$DESTINO"/
+echo "$(date) - Copia completada correctamente"
+# Eliminacion de directorio origen
+rm -rf "$ORIGEN"/*
